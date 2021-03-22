a73a964a0d94b4ebde344ffb94a2727902e3493d
User <User@FMVA77MWG>
2021-03-22 19:54:50 +0900

    test

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,17 @@
+import java.util.List;
 
 public class Main {
+	public static void main(String[] args) throws Exception {
+        String text = " ans1 = 10 + 20 ";
+        List<Token> tokens = new Lexer().init(text).tokenize();
+        for (Token token : tokens) {
+            System.out.println(token.toString());
+        }
+        // --> variable "ans1"
+        // --> sign "="
+        // --> digit "10"
+        // --> sign "+"
+        // --> digit "20"
+    }
 
 }
